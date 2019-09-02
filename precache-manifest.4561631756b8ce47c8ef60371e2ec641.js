b2af871ee5ab753cfbd3647345dd6170faf30532
svn1991 <svn1991@gmail.com>
2019-09-03 00:18:34 +0800

    Updates

---
+++ precache-manifest.4561631756b8ce47c8ef60371e2ec641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d709b9dc57fd38b319236b0fab99fd1a",
+    "url": "/svn1991/F1_World_Championships_Records/index.html"
+  },
+  {
+    "revision": "5ab2d000485dfa25d7b7",
+    "url": "/svn1991/F1_World_Championships_Records/static/css/main.4a0692fc.chunk.css"
+  },
+  {
+    "revision": "1c116752e1b0c5e688f0",
+    "url": "/svn1991/F1_World_Championships_Records/static/js/2.fd5194fd.chunk.js"
+  },
+  {
+    "revision": "5ab2d000485dfa25d7b7",
+    "url": "/svn1991/F1_World_Championships_Records/static/js/main.917a85c7.chunk.js"
+  },
+  {
+    "revision": "4959c91ff2515d492cf0",
+    "url": "/svn1991/F1_World_Championships_Records/static/js/runtime~main.79fcc3e9.js"
+  }
+]);
